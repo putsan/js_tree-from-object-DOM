0a5537095be4157cb659017aa231111da629c639
Yuriy Putsan <putsan57@gmail.com>
2020-10-16 14:20:31 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const list = document.createElement('ul');
+
+  element.append(list);
+
+  for (const key in data) {
+    const subList = document.createElement('li');
+
+    subList.textContent = key;
+    list.append(subList);
+
+    createTree(subList, data[key]);
+  }
+}
+
+createTree(tree, food);
